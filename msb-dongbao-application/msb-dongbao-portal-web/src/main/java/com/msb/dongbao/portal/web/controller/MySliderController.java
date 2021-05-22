0c7e9bd2d0cfdb642dc6323025ee74f8d42506b6
黄城 <9078881+huangchengkeepfighting@user.noreply.gitee.com>
2021-05-23 00:29:18 +0800

    SlideController

---
+++ msb-dongbao-application/msb-dongbao-portal-web/src/main/java/com/msb/dongbao/portal/web/controller/MySliderController.java
@@ -0,0 +1,34 @@
+package com.msb.dongbao.portal.web.controller;
+
+import com.msb.dongbao.portal.web.util.SliderUtil;
+import com.msb.dongbao.portal.web.util.VerificationVO;
+import com.ramostear.captcha.HappyCaptcha;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/my-slider")
+public class MySliderController {
+    @GetMapping("/generator")
+    public VerificationVO generatorCode(HttpServletRequest request, HttpServletResponse response) {
+
+        return SliderUtil.cut();
+    }
+
+    @GetMapping("/verify")
+    public String verify(String verifyCode, HttpServletRequest request) {
+
+
+//        Boolean aBoolean = kaptcha.validate(verifyCode);
+//        if (aBoolean) {
+//            HappyCaptcha.remove(request);
+//            return "验证码校验通过";
+//        }
+
+        return "验证码校验不通过";
+    }
+}
